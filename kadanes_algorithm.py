14a5ba1d6ee0fc798378e2b1dc4782e0d9aa7aa0
wotan <nabdel07@gmail.com>
2018-09-25 14:58:09 -0400

    Updated Kadane's function to also be able to retrieve subarray

--- kadanes_algorithm.py
+++ kadanes_algorithm.py
@@ -1,14 +1,31 @@
 class Kadane:
     @staticmethod
-    def get_max_contiguous_sum(arr: []):
+    def get_max_contiguous_sum_subarray(arr: []):
         """
-        :type arr: [] contains positive and negative integers
-        :rtype: int maximum contiguous sum
+        :param arr: Contains positive and negative integers
+        :return: Subarray containing the maximum contiguous sum
         """
 
+        start_pos = end_pos = marker = 0
         max_sum = curr_max = arr[0]
         for i in range(1, len(arr)):
-            curr_max = max(arr[i], arr[i] + curr_max)
-            max_sum = max(curr_max, max_sum)
+            if arr[i] > arr[i] + curr_max:
+                curr_max = arr[i]
+                marker = i
+            else:
+                curr_max = arr[i] + curr_max
 
-        return max_sum
+            if curr_max > max_sum:
+                max_sum = curr_max
+                end_pos = i
+                start_pos = marker
+
+        return arr[start_pos:end_pos + 1]
+
+    @staticmethod
+    def get_max_contiguous_sum(arr: []):
+        """
+        :type arr: [] Contains positive and negative integers
+        :rtype: int The maximum contiguous sum
+        """
+        return sum(Kadane.get_max_contiguous_sum_subarray(arr))
